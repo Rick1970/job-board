9df88f1e502f10ce616a605d7517f7cc846169d3
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-06 11:56:24 -0700

    Change grid layout and standardize color

--- js/backend.js
+++ js/backend.js
@@ -1,6 +1,6 @@
 //backend
 function displayPostings(i) {
-  var sentence = "<div class='back-job-postings col-md-4'><div class='panel panel-primary'><div class='panel-heading'><h3 class='panel-title'>" + backPositionArray[i].title + "</h3></div><div class='panel-body'><p><strong>Company: </strong>" + backPositionArray[i].company + "</p><p><strong>Description: </strong>" + backPositionArray[i].description + "</p><p>";
+  var sentence = "<div class='back-job-postings col-md-4'><div class='panel'><div class='panel-heading'><h3 class='panel-title'>" + backPositionArray[i].title + "</h3></div><div class='panel-body'><p><strong>Company: </strong>" + backPositionArray[i].company + "</p><p><strong>Description: </strong>" + backPositionArray[i].description + "</p><p>";
   for (var j = 0; j < backPositionArray[i].tags.length; j++) {
     if (j < backPositionArray[i].tags.length - 1) {
       sentence += "<strong>&#35;" +backPositionArray[i].tags[j] + "</strong>, ";
