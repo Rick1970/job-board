c9bfc3e9dcd5491345f187ba684b20d9366fb5ad
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-06 09:59:36 -0700

    Add minor styling changes to feature area

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@
 //     console.log("hi");
 // });
 
-$(".panel-heading").click(function() {
-  $(this).next().animate({
-    height: "toggle"
-  }, 300, function(){
-    console.log("hi");
-  });
-});
+// $(".panel-heading").click(function() {
+//   $(this).next().animate({
+//     height: "toggle"
+//   }, 300, function(){
+//     console.log("hi");
+//   });
+// });
 
 $("#sign-in-button").click(function() {
   $(".landing").fadeOut("fast", function() {
