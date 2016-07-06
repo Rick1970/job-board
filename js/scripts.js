7b36311c667ff3bef52a4c73943a1ae7b6ade5f9
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-06 09:23:02 -0700

    Add footer styling and remove some styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-$(".panel-heading").next().animate({
-  height: "toggle"
-}, 300, function(){
-    console.log("hi");
-});
+// $(".panel-heading").next().animate({
+//   height: "toggle"
+// }, 300, function(){
+//     console.log("hi");
+// });
 
 $(".panel-heading").click(function() {
   $(this).next().animate({
